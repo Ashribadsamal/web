a4a4c66488e7db5eed1466fba5008babd11665b0
Ashribadsamal <ashribad.samal11@gmail.com>
2020-09-19 15:32:23 -0700

    fi

---
+++ build/precache-manifest.e2dc0b8decfcda00a6f3d8501fbbb7cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cbf821f288f14124b95842ac34151d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a51d3909709fc7ca0da4",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "990877d6c02281c485ae",
+    "url": "/static/css/main.73e6f1bb.chunk.css"
+  },
+  {
+    "revision": "a51d3909709fc7ca0da4",
+    "url": "/static/js/2.aca4adfe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aca4adfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "990877d6c02281c485ae",
+    "url": "/static/js/main.ac0b4eae.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
